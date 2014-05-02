2366d6b5e65f943a35ccf1f7bda80f26393477d4
Kyle Blake <kyle@klkblake.com>
2014-05-02 15:55:17 +0930

    Add mmap and munmap syscalls

--- rts/syscalls.c
+++ rts/syscalls.c
@@ -2,10 +2,26 @@
 
 long syscall();
 
+char mmap_failed[] = "mmap failed\n";
 char write_failed[] = "Write failed\n";
 
 void *brk(void *addr) {
 	return (void *) syscall(0x0c, addr);
+}
+
+#define PROT_READ     0x01
+#define PROT_WRITE    0x02
+#define MAP_ANONYMOUS 0x20
+void *mmap(long length) {
+	long res = syscall(0x09, 0, length, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, -1);
+	if (res < 0) {
+		DIE(mmap_failed);
+	}
+	return (void *) res;
+}
+
+int munmap(void *addr, long length) {
+	return syscall(0x0b, addr, length);
 }
 
 long write(int fd, const char *buf, long size) {
