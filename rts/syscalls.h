09757ba7ef825392c29a7bbda0737f334876aff6
Kyle Blake <kyle@klkblake.com>
2014-05-02 15:54:38 +0930

    Fix off-by-one in DIE()

--- rts/syscalls.h
+++ rts/syscalls.h
@@ -1,6 +1,6 @@
 #define EINTR 4
 
-#define DIE(name) do { write(2, name, sizeof(name)); exit(1); } while (0)
+#define DIE(name) do { write(2, name, sizeof(name)-1); exit(1); } while (0)
 
 void *brk(void *addr);
 long write(int fd, const char *buf, long count);
