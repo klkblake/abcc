c902a9706b35832ebb03eba66eeb1e320eabce08
Kyle Blake <kyle@klkblake.com>
2014-11-12 16:46:57 +1030

    Fix incorrect argument type to syscalls

--- rts/syscalls.c
+++ rts/syscalls.c
@@ -1,12 +1,18 @@
 #include "syscalls.h"
 
-long syscall();
+long syscall(long n, long a, long b, long c, long d, long e, long f);
+#define syscall6(n, a, b, c, d, e, f) syscall(n, (long)(a), (long)(b), (long)(c), (long)(d), (long)(e), (long)(f))
+#define syscall5(n, a, b, c, d, e) syscall6(n, a, b, c, d, e, 0)
+#define syscall4(n, a, b, c, d) syscall5(n, a, b, c, d, 0)
+#define syscall3(n, a, b, c) syscall4(n, a, b, c, 0)
+#define syscall2(n, a, b) syscall3(n, a, b, 0)
+#define syscall1(n, a) syscall2(n, a, 0)
 
 char mmap_failed[] = "mmap failed\n";
 char write_failed[] = "Write failed\n";
 
 void *brk(void *addr) {
-	return (void *) syscall(0x0c, addr);
+	return (void *) syscall1(0x0c, addr);
 }
 
 #define PROT_READ     0x01
@@ -14,7 +20,7 @@ void *brk(void *addr) {
 #define MAP_PRIVATE   0x02
 #define MAP_ANONYMOUS 0x20
 void *mmap(long length) {
-	long res = syscall(0x09, 0, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	long res = syscall6(0x09, 0, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (res < 0) {
 		DIE(mmap_failed);
 	}
@@ -22,11 +28,11 @@ void *mmap(long length) {
 }
 
 int munmap(void *addr, long length) {
-	return syscall(0x0b, addr, length);
+	return syscall2(0x0b, addr, length);
 }
 
 long write(int fd, const char *buf, long size) {
-	int n = syscall(0x01, fd, buf, size);
+	int n = syscall3(0x01, fd, buf, size);
 	if (n == size) {
 		return 0;
 	}
@@ -47,5 +53,5 @@ void write_force(int fd, const char *buf, long size) {
 }
 
 void exit(long err) {
-	syscall(0x3c, err);
+	syscall1(0x3c, err);
 }
