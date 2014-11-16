abaa28bf23fe6c051e9f97221629dce87266a5d9
Kyle Blake <kyle@klkblake.com>
2014-11-17 00:06:20 +1030

    Dump graphs (only to file) only when specified on command line

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,10 @@ module Main where
 
 import Control.Monad.Morph
 import Control.Monad.ST
-import Control.Monad.State
+import Data.Maybe
+import Multiarg hiding (parse)
 import Pipes
 
-import System.Environment
 import System.IO
 import System.Exit
 
@@ -13,8 +13,58 @@ import Parser
 import TypeInferencer
 import Codegen
 
-raise :: Monad m => State s a -> StateT s m a
-raise = state . runState
+helpText :: String -> String
+helpText prog = unlines [ prog ++ " - A compiler for Awelon Bytecode"
+                        , ""
+                        , "Usage: " ++ prog ++ " [--typecheck] [--dump-<stage>[=FILE]]"
+                        , ""
+                        , "Options:"
+                        , "\t--typecheck              Run the typechecker instead of the compiler"
+                        , "\t--dump-<stage>[=FILE]    Dump the internal state of the typechecker at a particular stage to a GraphViz file"
+                        , "\t\tStages:"
+                        , "\t\tinitial        After the opcodes have had types assigned to them"
+                        , "\t\tunified        After the inputs and outputs of adjacent opcodes have been unified"
+                        , "\t\tresolved       After the substructural attributes have been resolved"
+                        , "\t\tsubstituted    After the variables have been eliminated from the terms"
+                        ]
+
+data Flag = TypeCheck
+          | Dump TIStage (Maybe FilePath)
+          | DumpAll
+          deriving (Eq, Show)
+
+flagSpecs :: [OptSpec Flag]
+flagSpecs =
+    [ OptSpec ["typecheck"]        [] (NoArg TypeCheck)
+    , OptSpec ["dump-initial"]     [] . OptionalArg $ return . Dump TIInitial
+    , OptSpec ["dump-unified"]     [] . OptionalArg $ return . Dump TIUnified
+    , OptSpec ["dump-resolved"]    [] . OptionalArg $ return . Dump TIResolved
+    , OptSpec ["dump-substituted"] [] . OptionalArg $ return . Dump TISubstituted
+    , OptSpec ["dump-all"]         [] $ NoArg DumpAll
+    ]
+
+data Options = Options { optDump :: [(TIStage, String)] }
+             deriving Show
+
+processFlags :: [Flag] -> Options
+processFlags flags =
+     let fs = if DumpAll `elem` flags
+                  then ($ flags) . foldr1 (.) $ map addIfMissing [minBound .. maxBound]
+                  else flags
+     in Options $ mapMaybe processDump fs
+  where
+    addIfMissing stage fs = if any (isDump stage) fs
+                                then fs
+                                else Dump stage Nothing:fs
+    isDump stage (Dump stage' _) = stage == stage'
+    isDump _ _ = False
+    processDump (Dump stage (Just file)) = Just (stage, file)
+    processDump (Dump stage Nothing)     = Just (stage, defaultFile stage)
+    processDump _ = Nothing
+    defaultFile TIInitial     = "initial.dot"
+    defaultFile TIUnified     = "unified.dot"
+    defaultFile TIResolved    = "resolved.dot"
+    defaultFile TISubstituted = "substituted.dot"
 
 doCompile :: IO ()
 doCompile = do
@@ -26,20 +76,22 @@ doCompile = do
                                   exitFailure
         Nothing -> exitFailure
 
-doTypeCheck :: IO ()
-doTypeCheck = do
+doTypeCheck :: Options -> IO ()
+doTypeCheck opts = do
     input <- getContents
     ops <- parse input
     case ops of
-        Just ops' -> print =<< runEffect (for (hoist stToIO $ inferTypes [minBound .. maxBound] ops') $ lift . putStrLn . snd)
+        Just ops' -> do
+            let dump = map fst $ optDump opts
+            res <- runEffect (for (hoist stToIO $ inferTypes dump ops') $ lift . writeGraph)
+            print res
         Nothing   -> exitFailure
+  where
+    writeGraph (stage, graph) = writeFile (fromJust . lookup stage $ optDump opts) graph
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        []          -> doCompile
-        ["compile"] -> doCompile
-        ["typecheck"] -> doTypeCheck
-        _ -> do putStrLn "Unrecognised arguments"
-                exitFailure
+    flags <- simpleHelp helpText flagSpecs Intersperse . const . Left $ ErrorMsg "Positional arguments are not allowed"
+    if TypeCheck `elem` flags
+        then doTypeCheck $ processFlags flags
+        else doCompile
