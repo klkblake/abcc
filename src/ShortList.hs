5950da44d67b4499d765623e394aa64d9b7d9ca0
Kyle Blake <kyle@klkblake.com>
2014-12-03 13:48:06 +1030

    Mark ShortList functions as inlinable

--- src/ShortList.hs
+++ src/ShortList.hs
@@ -19,12 +19,14 @@ err i n = error $ "Attempted to access non-existant index " ++ show i ++ " of " 
 (!) (ShortList _ _ b _) 1 = b
 (!) (ShortList _ _ _ c) 2 = c
 (!) (ShortList n _ _ _) i = err i n
+{-# INLINABLE (!) #-}
 
 update :: Int -> a -> ShortList a -> ShortList a
 update 0 !a (ShortList n _ b c) | n > 0 = ShortList n a b c
 update 1 !b (ShortList n a _ c) | n > 1 = ShortList n a b c
 update 2 !c (ShortList n a b _) | n > 2 = ShortList n a b c
 update i  _ (ShortList n _ _ _) = err i n
+{-# INLINABLE update #-}
 
 fromList :: [a] -> ShortList a
 fromList []           = ShortList 0 (err 0 0) (err 1 0) (err 2 0)
@@ -32,6 +34,7 @@ fromList [!a]         = ShortList 1 a         (err 1 1) (err 2 1)
 fromList [!a, !b]     = ShortList 2 a         b         (err 2 2)
 fromList [!a, !b, !c] = ShortList 3 a         b         c
 fromList _ = error "Attempted to create ShortList longer than 3 elements"
+{-# INLINABLE fromList #-}
 
 toList :: ShortList a -> [a]
 toList (ShortList 0 _ _ _) = []
@@ -39,3 +42,4 @@ toList (ShortList 1 a _ _) = [a]
 toList (ShortList 2 a b _) = [a, b]
 toList (ShortList 3 a b c) = [a, b, c]
 toList _ = error "Invalid ShortList"
+{-# INLINABLE toList #-}
